25a993658358c2f840172e0e94d51b5a16ab2571
unknown <793502404@qq.com>
2018-11-01 15:01:16 +0800

    标签页

--- index.js
+++ index.js
@@ -1,12 +1,2 @@
-$(".w5 .ui_title_wrap h3 a").click(function () {
-    if ($(this).parent().hasClass('on'))
-        return false;
-    else {
-        $(".w5 .ui_title_wrap h3").removeClass('on');
-        $(this).parent().addClass('on');
-        $(".w5 ul").hide();
-        $(".w5 ul:eq(" + $(this).parent().index() + ")").fadeIn();
-    }
-});
 
 
